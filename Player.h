9657f2f42639310a703295c377d95d9364421c89
Tran Chi Thanh <42613728+JackTVN@users.noreply.github.com>
2024-02-13 22:09:15 -0500

    Do a whole bunch of stuff
    I did some stuff, just want you to check if that seem right

--- Player.h
+++ Player.h
@@ -1,1 +1,24 @@
+#ifndef PLAYER_H
+#define PLAYER_H
 
+#include "Wheel.h"
+
+class Player 
+{
+    public:
+        Player(int startingMoney = 1000, int wheelValue = 10);
+
+        int release();
+        void changeWheelValue(int newValue);
+        void win(int winAmount);
+        void lose(int loseAmount);
+
+        int getMoney() { return m_money; }
+        void setMoney(int newMoney) { m_money = newMoney; }
+    private:
+        Wheel playerWheel;
+        int m_money;
+};
+
+
+#endif
--- Wheel.h
+++ Wheel.h
@@ -1,1 +1,17 @@
+#ifndef WHEEL_H
+#define WHEEL_H
 
+class Wheel 
+{
+    public:
+        Wheel(int maxVal = 10);
+        int Spin();
+
+        void setMax(int newMax);
+        int getMax() { return m_maxValue; }
+    private:
+        int m_maxValue;
+};
+
+
+#endif
