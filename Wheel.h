b981fad13c1f20796c159959bc6941356e5f1a05
margaretlyon21 <156958807+margaretlyon21@users.noreply.github.com>
2024-02-14 19:21:30 -0500

    Update Wheel.h

--- Wheel.h
+++ Wheel.h
@@ -1,1 +1,19 @@
+#ifndef WHEEL_H
+#define WHEEL_H
 
+class Wheel 
+{
+    public:
+        Wheel(int slots = 10);
+        void setSlots(int newSlots) {slots += newSlots;}
+        int getSlots(){return slots;}
+        void setLastHouseWin(bool houseWin) {LastHouseWin = houseWin;}
+        bool getLastHouseWin(){return LastHouseWin;}
+        int spinWheel();
+    private:
+        int slots;
+        bool LastHouseWin;
+};
+
+
+#endif
