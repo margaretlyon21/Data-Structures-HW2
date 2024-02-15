296d50b3a95e37ff30759d75b772101984236f47
margaretlyon21 <156958807+margaretlyon21@users.noreply.github.com>
2024-02-14 19:22:18 -0500

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,1 +1,19 @@
+#ifndef PLAYER_H
+#define PLAYER_H
 
+#include "Wheel.h"
+
+class Player 
+{
+    public:
+        Player(int startingMoney, int slots = 10);
+        int spinOurWheel();
+        int getMoney() { return money; }
+        void setMoney(int newMoney) { money += newMoney; }
+    private:
+        Wheel ourWheel;
+        int money;
+};
+
+
+#endif
