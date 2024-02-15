a8d9b96826623e3d7b9956f2cf1cd77f94c49bbb
margaretlyon21 <156958807+margaretlyon21@users.noreply.github.com>
2024-02-14 19:22:35 -0500

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <random>
+
+#include "Player.h"
+
+Player::Player(int startingMoney, int slots)
+{
+    money = startingMoney;
+    ourWheel = Wheel(slots);
+}
+
+int Player::spinOurWheel()
+{
+    return ourWheel.spinWheel();
+}
+
 
