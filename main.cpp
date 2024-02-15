340f5c6f5afd33d5aaa4f1ef6321b612372bf5dc
margaretlyon21 <156958807+margaretlyon21@users.noreply.github.com>
2024-02-14 19:21:00 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,39 +2,10 @@
 #include <random>
 using namespace std;
 
-class Wheel{
-    private:
-        int slots;
-    public:
-        //function to spin the roullete wheel with variable slot nuumber
-        int spinWheel(){ 
-            int result = rand() % slots + 1;
-            return result;
-        }
-        // wheel constructor
-        Wheel(int slots) : slots(slots) {}
-};
+#include "Player.h"
+#include "Wheel.h"
+#include "HardMode.h"
 
-class Player{
-    private:
-        int money;
-        //instance of wheel class
-        Wheel ourWheel;
-    public:
-        //getter and setter for money
-        int getMoney(){
-            return money;
-        }
-        void setMoney(int dollars){
-            money += dollars;
-        }
-        // function to spin the users wheel 
-        int spinOurWheel(){
-            return ourWheel.spinWheel();
-        }
-        //player constructor
-        Player(int money, int slots) : money(money), ourWheel(slots) {}
-};
 int main(){
     int slots, startingCash;
     bool correctSlots = false;
@@ -54,10 +25,133 @@ int main(){
         }
     }
     }
-    //starts a new game, calls constructor for the player
-    cout << "******************** Starting Game *********************\n";
+    //initializes the player and the house
     Player user(startingCash, slots);
     Player house(startingCash, slots);
+
+    //determines if the game will be easy or hard mode
+    cout << "Do you want to play easy mode (1) or hard mode (2)?";
+    int hardChoice;
+    cin >> hardChoice;
+    bool isHard = false;
+
+//starts a game in hard mode   
+if (hardChoice == 2){
+    //initializes the houses wheel
+    HardMode houseWheel(slots);
+    cout << "******************** Starting Game on Hard Mode *********************\n";
+
+
+    //loops while the player still has money
+    while (user.getMoney() > 0){
+        int bet;
+        cout << "Current money: $" << user.getMoney() << "\n";
+        cout << "Enter bet: ";
+        cin >> bet;
+
+        //checks if entered bet is valid 
+        if (bet > user.getMoney()){
+            cout << "You don't have enough money for that!";
+            continue;
+        }
+
+        //spins the wheel for the user and the house
+        int userSpin = user.spinOurWheel();
+        int houseSpin1 = houseWheel.Wheel::spinWheel();
+        int houseSpin2 = houseWheel.Wheel::spinWheel();
+        cout << "Spin Result: " << userSpin << endl;
+
+        //Allows the user to change wager, checks if entry is valid
+        char choice;
+        bool correctChoice = false;
+        while (!correctChoice){
+            cout << "Do you want to double (d), halve (h), or keep (k) the current wager?\n";
+            cin >> choice;
+                if (choice == 'd' | choice == 'h' | choice == 'k'){
+                    correctChoice = true;
+                }
+                else{
+                    cout << "Invalid input. Enter, d, h, or k! \n";
+                }    
+        }
+
+        bool win;
+        //doubles bet and checks who wins if user choses double
+        if (choice == 'd'){
+            bet *= 2;
+            cout << "First house spin: " << houseSpin1 << endl;
+            cout << "Second house spin: " << houseSpin2 << endl;
+            if (userSpin > houseSpin1 && userSpin > houseSpin2){
+                win = true;
+                user.setMoney(bet);
+            }
+            else{
+                win = false;
+                user.setMoney(-bet);
+            }
+        }
+        // halves bet and checks who wins if user choses half
+        else if (choice == 'h'){
+            bet /= 2;
+            cout << "First house spin: " << houseSpin1 << endl;
+            cout << "Second house spin: " << houseSpin2 << endl;
+            if (userSpin > houseSpin1 | userSpin > houseSpin2){
+                win = true;
+                user.setMoney(bet);
+            }
+            else{
+                win = false;
+                user.setMoney(-bet);
+            }
+        }
+        //checks who wins if no change to wager 
+        else {
+            cout << "House spin: " << houseSpin1 << endl;
+            if (userSpin > houseSpin1){
+                win = true;
+                user.setMoney(bet);
+            }
+            else{
+                win = false;
+                user.setMoney(-bet);
+            }
+        }
+
+        if (win){
+            cout << "Player wins! Current money: $" << user.getMoney() << endl;
+        }
+        else{
+            cout << "House wins! Current money: $" << user.getMoney() << endl;
+        }
+        houseWheel.adjustWheel(win);
+        cout << "The house currently has " << houseWheel.getSlots() << " slots!" << endl;
+
+        //if the user ran out of money, loop terminates
+        if (user.getMoney() <= 0){
+            cout << "You ran out of money.\n";
+            cout << "******************** Game Over *********************\n";
+            break;
+        }
+
+        //checks if the user wants to continue the game or cash out
+        char continueGame;
+        cout << "Do you want to continue playing? (y/n)";
+        cin >> continueGame;
+        if (continueGame != 'y' && continueGame != 'Y'){
+            cout << "******************** Game Over *********************\n";
+            cout << "You cashed out with $" << user.getMoney() <<".\n";
+            break;
+        }
+        else{
+            cout << "******************** Next Round *********************\n";
+        }
+    }
+}
+
+//starts a new game in easy mode
+    else{
+    cout << "******************** Starting Game *********************\n";
+
 
     //loops while the player still has money
     while (user.getMoney() > 0){
@@ -153,6 +247,7 @@ int main(){
         cout << "Do you want to continue playing? (y/n)";
         cin >> continueGame;
         if (continueGame != 'y' && continueGame != 'Y'){
+            cout << "******************** Game Over *********************\n";
             cout << "You cashed out with $" << user.getMoney() <<".\n";
             break;
         }
@@ -161,4 +256,8 @@ int main(){
         }
     }
 }
+}
 
+
+
+ 
