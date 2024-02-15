4e3a593fc5a6e149c3fc9f2a2dfc762887abd63c
margaretlyon21 <156958807+margaretlyon21@users.noreply.github.com>
2024-02-14 19:21:54 -0500

    Update Wheel.cpp

--- Wheel.cpp
+++ Wheel.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include <random>
 
+#include "Wheel.h"
+
+Wheel::Wheel(int newSlots)
+{
+    slots = newSlots;
+}
+
+int Wheel::spinWheel()
+{
+    return rand() % slots + 1;
+}
